9db7910ec363441abad6d5a4a6eb4134498478b9
feritarou <schwarz.f@web.de>
2024-09-18 13:24:33 +0200

    fix: remove more

--- challenges/1.TreasureHunt/main.js
+++
@@ -1,7 +0,0 @@
-export default function (base, { computed }) {
-  const derived = computed(() => base.reallyInterestingValue * 2)
-  return {
-    derived,
-    secondarySomething: 'wohoo!',
-  }
-}
